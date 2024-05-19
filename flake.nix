bb4af8cf5941945e78a285d85e619db648410931
Zebreus <lennart@zebre.us>
2024-05-19 17:33:44 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Master or machine";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }: flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        name = "master-or-machine";
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.nodejs ];
+          shellHook = ''
+            export PATH=$PATH:$PWD/node_modules/.bin
+            npm install
+          '';
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
