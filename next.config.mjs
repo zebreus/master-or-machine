f8469d1b2c050f5ccfc11ec9e7214efb2eeede8c
Zebreus <lennart@zebre.us>
2024-06-10 17:11:18 +0200

    Fix nextjs build

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    // Disabling this is probably a bad idea, see https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 export default nextConfig
