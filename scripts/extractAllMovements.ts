d0cda060474c0edf03035dea53e8eb58ab2e50ae
Zebreus <lennart@zebre.us>
2024-06-10 15:31:46 +0200

    Adjust writeDataFile to also generate json

--- scripts/extractAllMovements.ts
+++ scripts/extractAllMovements.ts
@@ -1,7 +1,7 @@
 import SparqlClient from "sparql-http-client"
 import z, { ZodType } from "zod"
-//import { writeDataFile } from "./helpers/writeDataFile"
-import { writeJsonFile } from "./helpers/writeDataToJson"
+import { writeDataFile } from "./helpers/writeDataFile"
+import { sparqlQuery } from "./helpers/sparqlQuery"
 
 interface ResultSchema {
   movementLabel: string
@@ -11,48 +11,8 @@ const resultSchema = z.object({
   movementLabel: z.string(),
 })
 
-// TODO: remove, once figured out, why sparqlQuery function produces build error
-const sparqlQueryTest = <ResultType>(
-  query: string,
-  resultSchema: ZodType<ResultType, any, any>,
-): Promise<ResultType[]> => {
-  const endpointUrl: string = "http://localhost:3030/art"
-  return new Promise((resolve, reject) => {
-    const client = new SparqlClient({ endpointUrl })
-    const stream = client.query.select(query)
-
-    const results: Array<ResultType> = []
-
-    stream.on("data", (result: unknown) => {
-      try {
-        // Simplify the result object to only contain the values
-        // Maybe you need to adjust this or create a new sparqlquery function for your use case
-        const simplifiedResult = Object.fromEntries(
-          Object.entries(z.record(z.string(), z.any()).parse(result)).map(
-            ([key, value]) => [key, value.value],
-          ),
-        )
-
-        const validatedResult = resultSchema.parse(simplifiedResult)
-
-        results.push(validatedResult)
-      } catch (error) {
-        reject(error)
-      }
-    })
-
-    stream.on("error", (err) => {
-      reject(err)
-    })
-
-    stream.on("end", () => {
-      resolve(results)
-    })
-  })
-}
-
 const getAllMovements = async () => {
-  const results = await sparqlQueryTest<ResultSchema>(
+  const results = await sparqlQuery<ResultSchema>(
     `
     PREFIX : <http://h-da.de/fbi/art/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -69,8 +29,7 @@ const getAllMovements = async () => {
 
   console.log(results)
 
-  //writeDataFile("allMovements", results, z.array(resultSchema))
-  writeJsonFile("allMovements", results)
+  await writeDataFile("allMovements", results, z.array(resultSchema))
 }
 
 getAllMovements()
